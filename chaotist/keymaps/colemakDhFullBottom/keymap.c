61e930cbb1af5fc2a7ccd89f775e046f68c44355
Tschibo00 <ballhause@gmx.de>
2024-04-21 18:45:21 +0200

    reworked lots of stuff, aligned with ToKeyO keymap

--- chaotist/keymaps/colemakDhFullBottom/keymap.c
+++ chaotist/keymaps/colemakDhFullBottom/keymap.c
@@ -15,16 +15,25 @@ enum custom_keycodes {
     MC_TILD,
     MC_LEAD,
     MC_COMMA,
-    MC_DOT
+    MC_DOT,
+    MC_EQL_BKSL,
+    MC_ASTR_PIP,
+    MC_PLUS_SLSH
 };
 
-const uint16_t PROGMEM cmb_auml[]=  {KC_A, KC_R,COMBO_END}; 
-const uint16_t PROGMEM cmb_ouml[]=  {KC_O, KC_I,COMBO_END}; 
-const uint16_t PROGMEM cmb_uuml[]=  {KC_U, KC_Y,COMBO_END}; 
-const uint16_t PROGMEM cmb_szlig[]= {KC_S, KC_T,COMBO_END}; 
-const uint16_t PROGMEM cmb_euro[]=  {KC_E, KC_O,COMBO_END}; 
-const uint16_t PROGMEM cmb_esc[]=   {KC_W, KC_F,COMBO_END}; 
-const uint16_t PROGMEM cmb_caps[]=  {KC_B, KC_J,COMBO_END}; 
+#define DELLINE KC_OUT
+const uint16_t PROGMEM cmb_auml[]=      {KC_A, KC_R,COMBO_END}; 
+const uint16_t PROGMEM cmb_ouml[]=      {KC_O, KC_I,COMBO_END}; 
+const uint16_t PROGMEM cmb_uuml[]=      {KC_U, KC_Y,COMBO_END}; 
+const uint16_t PROGMEM cmb_szlig[]=     {KC_R, KC_S,COMBO_END}; 
+const uint16_t PROGMEM cmb_euro[]=      {KC_E, KC_O,COMBO_END}; 
+const uint16_t PROGMEM cmb_esc[]=       {KC_W, KC_F,COMBO_END}; 
+const uint16_t PROGMEM cmb_caps[]=      {KC_B, KC_J,COMBO_END}; 
+const uint16_t PROGMEM cmb_tap[]=  		{KC_S, KC_T,COMBO_END}; 
+const uint16_t PROGMEM cmb_sfttab[]=	{KC_F, KC_P,COMBO_END}; 
+const uint16_t PROGMEM cmb_bksp[]=  	{KC_N, KC_E,COMBO_END}; 
+const uint16_t PROGMEM cmb_del[]=  		{KC_L, KC_U,COMBO_END}; 
+const uint16_t PROGMEM cmb_delline[]=  	{KC_H, MC_COMMA,COMBO_END}; 
 combo_t key_combos[]={
      COMBO(cmb_auml,    RALT(KC_Q)),
      COMBO(cmb_ouml,    RALT(KC_P)),
@@ -32,7 +41,12 @@ combo_t key_combos[]={
      COMBO(cmb_szlig,   RALT(KC_S)),
      COMBO(cmb_euro,    LALT(LCTL(KC_5))),
      COMBO(cmb_esc,     KC_ESC),
-     COMBO(cmb_caps,    QK_CAPS_WORD_TOGGLE)
+     COMBO(cmb_caps,    QK_CAPS_WORD_TOGGLE),
+     COMBO(cmb_tap,     KC_TAB),
+     COMBO(cmb_sfttab,  LSFT(KC_TAB)),
+     COMBO(cmb_bksp,    KC_BSPC),
+     COMBO(cmb_del,     KC_DEL),
+     COMBO(cmb_delline, DELLINE)					// workaround because qmk doesn't allow complex output for combos
 };
  
 LeaderOneKey leaderOneKeys[]={
@@ -47,20 +61,21 @@ LeaderOneKey leaderOneKeys[]={
     {KC_E,      "[]" SS_TAP(X_LEFT)},
     {KC_I,      "{}" SS_TAP(X_LEFT)},
     {KC_O,      "``" SS_TAP(X_LEFT)},
-    {KC_A,      "=>"},
-    {KC_C,      SS_TAP(X_CALC)},
-    {KC_X,      SS_DOWN(X_LGUI) "r" SS_UP(X_LGUI) SS_DELAY(100) "c:\\csp\\cmder\\Cmder.exe" SS_TAP(X_ENTER)},
-    {KC_Z,      SS_DOWN(X_LGUI) "r" SS_UP(X_LGUI) SS_DELAY(100) "c:\\csp\\cmder\\Cmder.exe" SS_DOWN(X_LSFT) SS_DOWN(X_LCTL) SS_TAP(X_ENTER) SS_UP(X_LCTL) SS_UP(X_LSFT)},
+    {KC_A,      "->"},
     {MC_QUOT,   "\"\"" SS_TAP(X_LEFT)},
     {KC_LCTL,   SS_DOWN(X_LSFT) SS_DOWN(X_LGUI) SS_TAP(X_LEFT) SS_UP(X_LGUI) SS_UP(X_LSFT)},
     {KC_MINUS,  SS_DOWN(X_LALT) SS_TAP(X_F4) SS_UP(X_LALT)},
     {KC_BSLS,   SS_DOWN(X_LALT) SS_DOWN(X_LCTL) SS_TAP(X_DEL) SS_UP(X_LCTL) SS_UP(X_LALT)},
     {KC_SLSH,   SS_DOWN(X_LSFT) SS_DOWN(X_LGUI) SS_TAP(X_RIGHT) SS_UP(X_LGUI) SS_UP(X_LSFT)},
-    {KC_D,      SS_DOWN(X_LCTL) SS_TAP(X_C) SS_UP(X_LCTL)},
-    {KC_V,      SS_DOWN(X_LCTL) SS_TAP(X_X) SS_UP(X_LCTL)},
+    {KC_C,      SS_DOWN(X_LCTL) SS_TAP(X_C) SS_UP(X_LCTL)},
+    {KC_D,      SS_DOWN(X_LCTL) SS_TAP(X_X) SS_UP(X_LCTL)},
     {KC_K,      SS_DOWN(X_LALT) SS_DOWN(X_LCTL) SS_TAP(X_V) SS_UP(X_LCTL) SS_UP(X_LALT)},
-    {KC_H,      SS_DOWN(X_LCTL) SS_TAP(X_V) SS_UP(X_LCTL)},
-    {KC_COMMA,  SS_DOWN(X_LSFT) SS_DOWN(X_LCTL) SS_TAP(X_V) SS_UP(X_LCTL) SS_UP(X_LSFT)},
+    {KC_V,      SS_DOWN(X_LCTL) SS_TAP(X_V) SS_UP(X_LCTL)},
+    {KC_H,      SS_DOWN(X_LSFT) SS_DOWN(X_LCTL) SS_TAP(X_V) SS_UP(X_LCTL) SS_UP(X_LSFT)},
+	{KC_BSPC,   SS_TAP(X_END) SS_DOWN(X_LSFT) SS_TAP(X_HOME) SS_TAP(X_HOME) SS_UP(X_LSFT) SS_TAP(X_DELETE) SS_TAP(X_DELETE)},
+    {KC_F,      SS_TAP(X_LSFT) SS_TAP(X_LSFT)},
+    {KC_TAB,    SS_DOWN(X_LALT) SS_TAP(X_TAB) SS_UP(X_LALT)},
+    {KC_ENT,    SS_DOWN(X_LSFT) SS_DOWN(X_LALT) SS_TAP(X_TAB) SS_UP(X_LALT) SS_UP(X_LSFT)}
 };
 
 // these are defined in lk_words.h, example:
@@ -90,7 +105,7 @@ uint16_t MC_LEAD_TIMER = 0;
  * │           │     │     │     │     │     │     │     │     │           │
  * ├────────┬──┴──┬──┴──┬──┴─────┴──┬──┴──┬──┴─────┴──┬──┴──┬──┴──┬────────┤
  * │        │     │sys  │space      │lead │     return│sys  │\ |  │/ ?     │
- * │Ctrl    │Alt  │     │num sym    │Shift│     nav fn│     │     │        │
+ * │Ctrl    │Alt  │     │shift      │numsy│     nav fn│     │     │        │
  * └────────┴─────┴─────┴───────────┴─────┴───────────┴─────┴─────┴────────┘
  * number/symbol
  * ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
@@ -106,7 +121,7 @@ uint16_t MC_LEAD_TIMER = 0;
  * │           │     │     │     │     │     │     │     │     │           │
  * ├────────┬──┴──┬──┴──┬──┴─────┴──┬──┴──┬──┴─────┴──┬──┴──┬──┴──┬────────┤
  * │        │     │     │space      │lead │     return│     │~    │:       │
- * │Ctrl    │Alt  │     │num sym    │Shift│     nav fn│     │     │        │
+ * │Ctrl    │Alt  │     │shift      │numsy│     nav fn│     │     │        │
  * └────────┴─────┴─────┴───────────┴─────┴───────────┴─────┴─────┴────────┘
  * navigation/function
  * ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
@@ -116,13 +131,13 @@ uint16_t MC_LEAD_TIMER = 0;
  * │     │     ├─────┤F8   ├─────┤     │     ├─────┤↓    ├─────┤     │     │
  * │     ├─────┤F7   │     │F9   ├─────┼─────┤←    │     │→    ├─────┤     │
  * │     │F6   │     ├─────┤     │F10  │Home │     ├─────┤     │End  │     │
- * │     │     ├─────┤     ├─────┤     │     ├─────┤     ├─────┤     │     │
- * ├─────┴─────┤F12  │     │     ├─────┼─────┤WrdLf│     │WrdRg├─────┴─────┤
- * │      F11  │     │     │     │     │SelWL│     │     │     │SelWR      │
+ * │     │     ├─────┤CtrlC├─────┤     │     ├─────┤     ├─────┤     │     │
+ * ├─────┴─────┤F12  │     │CtrlX├─────┼─────┤WrdLf│     │WrdRg├─────┴─────┤
+ * │      F11  │     │     │     │CtrlV│SelWL│     │     │     │SelWR      │
  * │           │     │     │     │     │     │     │     │     │           │
  * ├────────┬──┴──┬──┴──┬──┴─────┴──┬──┴──┬──┴─────┴──┬──┴──┬──┴──┬────────┤
  * │        │     │     │space      │lead │     return│     │     │        │
- * │Ctrl    │Alt  │     │num sym    │Shift│     nav fn│     │     │        │
+ * │Ctrl    │Alt  │     │shift      │numsy│     nav fn│     │     │        │
  * └────────┴─────┴─────┴───────────┴─────┴───────────┴─────┴─────┴────────┘
  * leader one keys (for two-or-more leader key combos see lk_words.h)
  * ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
@@ -131,14 +146,14 @@ uint16_t MC_LEAD_TIMER = 0;
  * │     │     │     ├─────┤     │     │     │Code ├─────┤     │     │     │
  * │     │     ├─────┤PrtSc├─────┤     │     ├─────┤[]   ├─────┤     │     │
  * │     ├─────┤     │     │     ├─────┼─────┤()   │     │{}   ├─────┤     │
- * │     │=>   │     ├─────┤     │     │- [ ]│     ├─────┤     │     │     │
- * │     │     ├─────┤Calc ├─────┤     │     ├─────┤Shift├─────┤     │     │
- * ├─────┴─────┤Cmder│     │CtrlC├─────┼─────┤CtrlV│CtrlV│     ├─────┴─────┤
- * │      Amd  │     │     │     │CtrlX│Alt  │     │     │     │""         │
- * │      Cmd  │     │     │     │     │CtrlV│     │     │     │           │
+ * │     │->   │     ├─────┤     │     │- [ ]│     ├─────┤     │     │     │
+ * │     │     ├─────┤CtrlC├─────┤     │     ├─────┤     ├─────┤     │     │
+ * ├─────┴─────┤     │     │CtrlX├─────┼─────┤Shift│     │     ├─────┴─────┤
+ * │           │     │     │     │CtrlV│Alt  │CtrlV│     │     │""         │
+ * │           │     │     │     │     │CtrlV│     │     │     │           │
  * ├────────┬──┴──┬──┴──┬──┴─────┴──┬──┴──┬──┴─────┴──┬──┴──┬──┴──┬────────┤
  * │WinLf   │AltF4│     │           │lead │           │     │CtlAl│WinRg   │
- * │        │     │     │           │Shift│           │     │Del  │        │
+ * │        │     │     │           │numsy│           │     │Del  │        │
  * └────────┴─────┴─────┴───────────┴─────┴───────────┴─────┴─────┴────────┘
  * system
  * ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
@@ -159,41 +174,41 @@ uint16_t MC_LEAD_TIMER = 0;
  * combos
  * ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
  * │     │     │  +=Esc=+  │     │     │     │     │  +==ü==+  │     │     │
- * │     │     │     │     │     │     │     │     │     │     │     │     │
+ * │     │     │     │ +SftTab+  │     │     │  +=Del=+  │     │     │     │
  * │     │     │     ├─────┤     │     │     │     ├─────┤     │     │     │
  * │     │     ├─────┤     ├─────┤     │     ├─────┤     ├─────┤     │     │
- * │     ├─────┤     │  +==ß==+  ├─────┼─────┤     │  +=====€==\─────┤     │
+ * │     ├─────┤  +==ß==+=Tab=+  ├─────┼─────┤ +=BkSp=+=====€==\─────┤     │
  * │     │  +==ä==+  ├─────┤     │     │     │     ├─────┤  +=ö \=+  │     │
  * │     │     ├─────┤     ├─────┤     │     ├─────┤     ├─────\==+  │     │
- * ├─────┴─────┤     │     │     ├─────┼─────┤     │     │     ├─────┴─────┤
+ * ├─────┴─────┤     │     │     ├─────┼─────┤+=DelLine=+│     ├─────┴─────┤
  * │           │     │     │     │     │     │     │     │     │           │
  * │           │     │     │     │     │     │     │     │     │           │
  * ├────────┬──┴──┬──┴──┬──┴─────┴──┬──┴──┬──┴─────┴──┬──┴──┬──┴──┬────────┤
- * │        │     │     │    +======system=======+    │     │     │        │
+ * │        │     │     │           │     │           │     │     │        │
  * │        │     │     │           │     │           │     │     │        │
  * └────────┴─────┴─────┴───────────┴─────┴───────────┴─────┴─────┴────────┘ 
  */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT(
-    KC_TAB,         KC_Q,   KC_W,   KC_F,       KC_P,                KC_B,      KC_J,               KC_L,             KC_U,       KC_Y,          KC_MINUS,    KC_BSPC,
-    KC_NO,          KC_A,   KC_R,   KC_S,       KC_T,                KC_G,      KC_M,               KC_N,             KC_E,       KC_I,          KC_O,        KC_NO,
-    KC_Z,           KC_NO,  KC_X,   KC_C,       KC_D,                KC_V,      KC_K,               KC_H,             MC_COMMA,   MC_DOT,        KC_NO,       MC_QUOT,
-    KC_LCTL,        KC_NO,  KC_LALT,MO(_SYSTEM),LT(_NUMSYM,KC_SPACE),MC_LEAD,   KC_NO,              LT(_NAVFN,KC_ENT),MO(_SYSTEM),KC_BSLS,       KC_NO,       KC_SLSH),
+    KC_TAB,         KC_Q,   KC_W,   KC_F,       KC_P,                KC_B,        KC_J,               KC_L,             KC_U,       KC_Y,          KC_MINUS,    KC_BSPC,
+    KC_NO,          KC_A,   KC_R,   KC_S,       KC_T,                KC_G,        KC_M,               KC_N,             KC_E,       KC_I,          KC_O,        KC_NO,
+    KC_Z,           KC_NO,  KC_X,   KC_C,       KC_D,                KC_V,        KC_K,               KC_H,             MC_COMMA,   MC_DOT,        KC_NO,       MC_QUOT,
+    KC_LCTL,        KC_NO,  KC_LALT,MO(_SYSTEM),LSFT_T(KC_SPACE),    MC_LEAD,     KC_NO,              LT(_NAVFN,KC_ENT),MO(_SYSTEM),KC_BSLS,       KC_NO,       KC_SLSH),
 [_NUMSYM] = LAYOUT(
-    KC_TAB,         KC_6,   KC_7,   KC_8,       KC_9,                KC_0,      KC_1,               KC_2,             KC_3,       KC_4,          KC_5,        KC_DEL,
-    KC_NO,          KC_EXLM,KC_AT,  KC_EQL,     KC_ASTR,             KC_PLUS,   KC_COLON,           KC_LPRN,          KC_LBRC,    KC_LCBR,       KC_LT,       KC_NO,
-    KC_DLR,         KC_NO,  KC_PERC,MC_CARET,   KC_HASH,             KC_AMPR,   KC_SCLN,            KC_RPRN,          KC_RBRC,    KC_RCBR,       KC_NO,       KC_GT,
-    KC_LCTL,        KC_NO,  KC_LALT,KC_NO,      LT(_NUMSYM,KC_SPACE),MC_LEAD,   KC_NO,              LT(_NAVFN,KC_ENT),KC_NO,      MC_TILD,       KC_NO,       KC_COLON),
+    KC_TAB,         KC_6,   KC_7,   KC_8,       KC_9,                KC_0,        KC_1,               KC_2,             KC_3,       KC_4,          KC_5,        KC_DEL,
+    KC_NO,          MC_TILD,KC_AT,  MC_EQL_BKSL,MC_ASTR_PIP,         MC_PLUS_SLSH,KC_COLON,           KC_LPRN,          KC_LBRC,    KC_LCBR,       KC_LT,       KC_NO,
+    KC_DLR,         KC_NO,  KC_PERC,MC_CARET,   KC_HASH,             KC_AMPR,     KC_SCLN,            KC_RPRN,          KC_RBRC,    KC_RCBR,       KC_NO,       KC_GT,
+    KC_LCTL,        KC_NO,  KC_LALT,KC_NO,      LSFT_T(KC_SPACE),    MC_LEAD,     KC_NO,              LT(_NAVFN,KC_ENT),KC_NO,      MC_TILD,       KC_NO,       KC_COLON),
 [_NAVFN] = LAYOUT(
-    KC_TAB,         KC_F1,  KC_F2,  KC_F3,      KC_F4,               KC_F5,     LCTL(KC_HOME),      KC_PGUP,          KC_UP,      KC_PGDN,       LCTL(KC_END),KC_DEL,
-    KC_NO,          KC_F6,  KC_F7,  KC_F8,      KC_F9,               KC_F10,    KC_HOME,            KC_LEFT,          KC_DOWN,    KC_RIGHT,      KC_END,      KC_NO,
-    KC_F11,         KC_NO,  KC_F12, KC_NO,      KC_NO,               KC_NO,     LSFT(LCTL(KC_LEFT)),LCTL(KC_LEFT),    KC_NO,      LCTL(KC_RIGHT),KC_NO,       LSFT(LCTL(KC_RIGHT)),
-    KC_LCTL,        KC_NO,  KC_LALT,KC_NO,      LT(_NUMSYM,KC_SPACE),MC_LEAD,   KC_NO,              LT(_NAVFN,KC_ENT),KC_NO,      KC_NO,         KC_NO,       KC_NO),
+    KC_TAB,         KC_F1,  KC_F2,  KC_F3,      KC_F4,               KC_F5,       LCTL(KC_HOME),      KC_PGUP,          KC_UP,      KC_PGDN,       LCTL(KC_END),KC_DEL,
+    KC_NO,          KC_F6,  KC_F7,  KC_F8,      KC_F9,               KC_F10,      KC_HOME,            KC_LEFT,          KC_DOWN,    KC_RIGHT,      KC_END,      KC_NO,
+    KC_F11,         KC_NO,  KC_F12, LCTL(KC_C), LCTL(KC_X),          LCTL(KC_V),  LSFT(LCTL(KC_LEFT)),LCTL(KC_LEFT),    KC_NO,      LCTL(KC_RIGHT),KC_NO,       LSFT(LCTL(KC_RIGHT)),
+    KC_LCTL,        KC_NO,  KC_LALT,KC_NO,      LSFT_T(KC_SPACE),    MC_LEAD,     KC_NO,              LT(_NAVFN,KC_ENT),KC_NO,      KC_NO,         KC_NO,       KC_NO),
 [_SYSTEM] = LAYOUT(
-    KC_SYSTEM_WAKE, QK_BOOT,KC_MPRV,KC_MPLY,    KC_MNXT,             KC_VOLU,   KC_MS_WH_UP,        KC_MS_WH_LEFT,    KC_MS_UP,   KC_MS_WH_RIGHT,KC_MS_ACCEL2,AC_TOGG,
-    KC_NO,          KC_NO,  KC_NO,  KC_MSTP,    KC_NO,               KC_VOLD,   KC_MS_WH_DOWN,      KC_MS_LEFT,       KC_MS_DOWN, KC_MS_RIGHT,   KC_MS_ACCEL1,KC_NO,
-    KC_SYSTEM_SLEEP,KC_NO,  KC_NO,  KC_NO,      KC_NO,               KC_MUTE,   KC_NO,              KC_MS_BTN1,       KC_MS_BTN3, KC_MS_BTN2,    KC_NO,       KC_MS_ACCEL0,
-    KC_SYSTEM_POWER,KC_NO,  KC_NO,  MO(_SYSTEM),KC_NO,               KC_NO,     KC_NO,              KC_NO,            MO(_SYSTEM),KC_NO,         KC_NO,       KC_NO)
+    KC_SYSTEM_WAKE, QK_BOOT,KC_MPRV,KC_MPLY,    KC_MNXT,             KC_VOLU,     KC_MS_WH_UP,        KC_MS_WH_LEFT,    KC_MS_UP,   KC_MS_WH_RIGHT,KC_MS_ACCEL2,AC_TOGG,
+    KC_NO,          KC_NO,  KC_NO,  KC_MSTP,    KC_NO,               KC_VOLD,     KC_MS_WH_DOWN,      KC_MS_LEFT,       KC_MS_DOWN, KC_MS_RIGHT,   KC_MS_ACCEL1,KC_NO,
+    KC_SYSTEM_SLEEP,KC_NO,  KC_NO,  KC_NO,      KC_NO,               KC_MUTE,     KC_NO,              KC_MS_BTN1,       KC_MS_BTN3, KC_MS_BTN2,    KC_NO,       KC_MS_ACCEL0,
+    KC_SYSTEM_POWER,KC_NO,  KC_NO,  MO(_SYSTEM),KC_NO,               KC_NO,       KC_NO,              KC_NO,            MO(_SYSTEM),KC_NO,         KC_NO,       KC_NO)
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -205,11 +220,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case MC_CARET:  if (record->event.pressed) SEND_STRING(SS_LSFT("6") SS_TAP(X_SPC));                                 break;
         case MC_LEAD:
             if (record->event.pressed) {
-                register_code(KC_LSFT);
+                layer_on(_NUMSYM);
                 leaderKeyPressed = false; 
                 MC_LEAD_TIMER = timer_read();
             } else {
-                unregister_code(KC_LSFT);
+                layer_off(_NUMSYM);
                 if (leaderKeyPressed) break;
                 if (timer_elapsed(MC_LEAD_TIMER) < TAPPING_TERM) leader_start();
             }
@@ -237,6 +252,45 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 }
 			}
             break;
+        case MC_EQL_BKSL:
+            if (record->event.pressed && !leader_sequence_active()) {
+				if (mods & MOD_MASK_SHIFT) {
+                    unregister_code(KC_LSFT);
+                    SEND_STRING("\\"); 
+                    register_code(KC_LSFT);
+                } else {
+                    SEND_STRING("=");
+                }
+			}
+            break;
+        case MC_ASTR_PIP:
+            if (record->event.pressed && !leader_sequence_active()) {
+				if (mods & MOD_MASK_SHIFT) {
+                    unregister_code(KC_LSFT);
+                    SEND_STRING("|"); 
+                    register_code(KC_LSFT);
+                } else {
+                    SEND_STRING("*");
+                }
+			}
+            break;
+        case MC_PLUS_SLSH:
+            if (record->event.pressed && !leader_sequence_active()) {
+				if (mods & MOD_MASK_SHIFT) {
+                    unregister_code(KC_LSFT);
+                    SEND_STRING("/"); 
+                    register_code(KC_LSFT);
+                } else {
+                    SEND_STRING("+");
+                }
+			}
+            break;
+		case DELLINE:
+			if (record->event.pressed && !leader_sequence_active()) {
+				SEND_STRING(SS_TAP(X_END) SS_DOWN(X_LSFT) SS_TAP(X_HOME) SS_TAP(X_HOME) SS_UP(X_LSFT) SS_TAP(X_DELETE) SS_TAP(X_DELETE));
+				return false;
+			}
+			break;
     }
     return true;
 };
@@ -257,8 +311,10 @@ void leader_end_user(void) {
     // everything not handled falls through and can be special-handled here
     set_mods(mods);
 }
+/*
 
 layer_state_t layer_state_set_user(layer_state_t state) {
 	state=update_tri_layer_state(state, _NUMSYM, _NAVFN, _SYSTEM);
 	return state;
 }
+*/
