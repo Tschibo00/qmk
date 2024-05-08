b0913cd070f0579e9c4cba21168fd91c8bcb447e
Tschibo00 <ballhause@gmx.de>
2024-05-08 17:34:49 +0200

    Add files via upload

--- tokeyo/config.h
+++ tokeyo/config.h
@@ -2,8 +2,7 @@
 
 #define COMBO_STRICT_TIMER
 #define COMBO_TERM 30
-//#define HOLD_ON_OTHER_KEY_PRESS 
-//#define PERMISSIVE_HOLD 
+#define PERMISSIVE_HOLD 
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
 #define LEADER_PER_KEY_TIMING
@@ -11,3 +10,16 @@
 #define LEADER_NO_TIMEOUT
 #define CAPS_WORD_IDLE_TIMEOUT 3000
 #define TAPPING_TERM 300
+#define QUICK_TAP_TERM 0
+
+#define RGBLIGHT_SLEEP 			// turn off effects when suspended
+#define RGBLIGHT_DEFAULT_HUE 135
+#define RGBLED_NUM 1
+#define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_STATIC_LIGHT
+#define RGBLIGHT_DEFAULT_ON true
+
+// order: static, breathe, rainbow, alternating, twinkle
+#define RGBLIGHT_EFFECT_ALTERNATING				// 1 mode
+#define RGBLIGHT_EFFECT_BREATHING				// 4 modes
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD			// 3 modes
+#define RGBLIGHT_EFFECT_TWINKLE					// 6 modes
