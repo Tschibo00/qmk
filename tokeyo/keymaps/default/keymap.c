344803500ebc5992935dc7720ad51e15906b2435
Tschibo00 <ballhause@gmx.de>
2024-05-10 16:34:54 +0200

    corrected keymap.c (plus spacing of code)

--- tokeyo/keymaps/default/keymap.c
+++ tokeyo/keymaps/default/keymap.c
@@ -36,12 +36,12 @@ const uint16_t PROGMEM cmb_szlig[]= 	{KC_R, 	   KC_S,	COMBO_END};
 const uint16_t PROGMEM cmb_euro[]=  	{KC_E, 	   KC_O,	COMBO_END}; 
 const uint16_t PROGMEM cmb_esc[]=   	{KC_W, 	   KC_F,	COMBO_END}; 
 const uint16_t PROGMEM cmb_caps[]=  	{KC_B, 	   KC_J,	COMBO_END}; 
-const uint16_t PROGMEM cmb_tap[]=  		{KC_S, 	   KC_T,	COMBO_END}; 
+const uint16_t PROGMEM cmb_tap[]=  	{KC_S, 	   KC_T,	COMBO_END}; 
 const uint16_t PROGMEM cmb_sfttab[]=	{KC_F, 	   KC_P,	COMBO_END}; 
 const uint16_t PROGMEM cmb_bksp[]=  	{KC_N, 	   KC_E,	COMBO_END}; 
-const uint16_t PROGMEM cmb_del[]=  		{KC_L, 	   KC_U,	COMBO_END}; 
-const uint16_t PROGMEM cmb_delline[]=  	{KC_H, 	   MC_COMMA,COMBO_END};
-const uint16_t PROGMEM cmb_delword[]=  	{MC_COMMA, KC_DOT, 	COMBO_END};
+const uint16_t PROGMEM cmb_del[]=  	{KC_L, 	   KC_U,	COMBO_END}; 
+const uint16_t PROGMEM cmb_delline[]=  	{KC_H, 	   MC_COMMA,    COMBO_END};
+const uint16_t PROGMEM cmb_delword[]=  	{MC_COMMA, MC_DOT, 	COMBO_END};
 combo_t key_combos[]={
      COMBO(cmb_auml,    RALT(KC_Q)),
      COMBO(cmb_ouml,    RALT(KC_P)),
@@ -55,7 +55,7 @@ combo_t key_combos[]={
      COMBO(cmb_bksp,    KC_BSPC),
      COMBO(cmb_del,     KC_DEL),
      COMBO(cmb_delline, DELLINE),					// workaround because qmk doesn't allow complex output for combos
-	 COMBO(cmb_delword, DELWORD)
+     COMBO(cmb_delword, DELWORD)
 };
 
 LeaderOneKey leaderOneKeys[]={
